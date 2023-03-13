d881692dc62893779e3325fc82c64e3b41acf6ae
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-13 19:21:17 +0300

    Add domain and client id

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'wube-fsnd.us.auth0.com',            // Auth0 domain
+  clientId: 'sxDMBXruqYSq4gS58X3TC0uDC8oJbEXr',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
