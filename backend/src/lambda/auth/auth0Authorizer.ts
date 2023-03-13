ec5f760b34f6f98b3cbd39c07c36fc47db7580f1
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-13 15:46:21 +0300

    Implement the authentication in the auth0Authorizer.ts

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -10,9 +10,10 @@ import { JwtPayload } from '../../auth/JwtPayload'
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
+// ** DONE
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://wube-fsnd.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -61,8 +62,39 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
+  
+  /* 
+*Here are the steps for validating the JWT:*
 
-  return undefined
+  Retrieve the JWKS and filter for potential signature verification keys.
+  Extract the JWT from the request's authorization header.
+  Decode the JWT and grab the kid property from the header.
+  Find the signature verification key in the filtered JWKS with a matching kid property.
+  Using the x5c property build a certificate which will be used to verify the JWT signature.
+  Ensure the JWT contains the expected audience, issuer, expiration, etc.
+  */
+
+
+ const response = await Axios.get(jwksUrl)
+ const keys = response.data.keys
+ const signingKeys = keys.find(key => key.kid == jwt.header.kid)
+ logger.info('signingKeys', signingKeys)
+  if (!signingKeys) {
+    throw new Error('The JWKS endpoint did not contain any keys')
+  }
+
+  // build certificate
+  const pemData = signingKeys.x5c[0]
+  const pem = `-----BEGIN CERTIFICATE-----\n${pemData}\n-----END CERTIFICATE-----`
+
+  // verify token
+  // Ensure the JWT contains the expected audience, issuer, expiration, etc.
+  const verifiedToken = verify(token, pem, { algorithms: ['RS256'] }) as JwtPayload
+  logger.info('verifiedToken', verifiedToken)
+  return verifiedToken
+
+
+  // return undefined
 }
 
 function getToken(authHeader: string): string {
